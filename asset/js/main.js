d94a40e9481f340d6ba84170be974956f64f72c7
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-07-29 13:13:41 +0900

    changed email source location / added scroll reveal aniamtion

--- asset/js/main.js
+++ asset/js/main.js
@@ -43,3 +43,47 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+/*================== Scroll Reveal Animation ==================*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(`.data__item`, {
+  interval: 200,
+});
+
+/*================== Email ==================*/
+(function () {
+  emailjs.init("user_lIEcZi4awRHqgiziPAV3Z");
+})();
+
+const btnSend = document.getElementById("btnSend");
+btnSend.addEventListener("click", (e) => {
+  e.preventDefault();
+  let templateParams = {
+    toName: document.getElementById("firstName").value,
+    fromName: document.getElementById("lastName").value,
+    replyTo: document.getElementById("email").value,
+    message: document.getElementById("messageBox").value,
+  };
+  console.log(templateParams);
+  sendEmail(templateParams);
+});
+
+function sendEmail(templateParams) {
+  emailjs.send("service_tui1gcl", "template_p1fingl", templateParams).then(
+    function (response) {
+      alert("메일을 성공적으로 보냈습니다.");
+      console.log("SUCCESS!", response.status, response.text);
+      location.reload();
+    },
+    function (error) {
+      alert("시스템 점검 중입니다.");
+      console.log("FAILED...", error);
+    }
+  );
+}
