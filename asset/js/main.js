8567b1f2dba4da83fefaa8aed7ee01ec1a0303a1
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-12-21 00:05:13 +0900

    최신 버전

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,20 +1,19 @@
-'use strict'
+"use strict";
 
 /* SHOW MENU  */
-const showMenu = () =>{
-  const toggle = document.getElementById('nav-toggle'),
-        nav = document.getElementById('nav-menu');
+const showMenu = () => {
+  const toggle = document.getElementById("nav-toggle"),
+    nav = document.getElementById("nav-menu");
 
-        if(toggle && nav){
-          toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show-menu');
-            console.log("toggle");
-          })
-        }
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+      console.log("toggle");
+    });
+  }
 };
 
 showMenu();
-
 
 /* SLIDE SHOW */
 var slideIndex = 1;
@@ -22,26 +21,30 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
